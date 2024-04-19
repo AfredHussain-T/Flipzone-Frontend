ff628e9f19ed93052197b344e040c348a3a7572b
AfredHussain-T <afredhussain69@gmail.com>
2024-04-19 14:46:38 +0530

    frontend-deploying

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { useAuth } from "../../context/auth";
-import { token } from "morgan";
 import toast from 'react-hot-toast';
 import SearchInput from "../Form/SearchInput";
 import useCategory from "../../hooks/useCategory";
